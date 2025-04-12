f885c6d77913249c6785cd1034b970c99f7d4a63
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2025-04-12 17:56:11 +0300

    media testimony

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,20 +4,21 @@ import {FlexContainer} from "../../../components/FlexContainer";
 import {Slider} from "../../../components/slider/Slider";
 import {Icon} from "../../../components/icon/Icon";
 import {IconWrapper} from "../skills/skill/Skill.tsx";
+import {Container} from "../../../components/Container.tsx";
 
 
 export const Testimony = () => {
     return (
         <StiledTestimony>
-
-            <SectionTitle>Testimony</SectionTitle>
-            <FlexContainer direction={'column'} align={'center'}>
-                <IconWrapper>
-                    <Icon iconId={'quote'}/>
-                </IconWrapper>
-                <Slider/>
-            </FlexContainer>
-
+            <Container>
+                <SectionTitle>Testimony</SectionTitle>
+                <FlexContainer direction={'column'} align={'center'}>
+                    <IconWrapper>
+                        <Icon iconId={'quote'}/>
+                    </IconWrapper>
+                    <Slider/>
+                </FlexContainer>
+            </Container>
         </StiledTestimony>
     );
 };

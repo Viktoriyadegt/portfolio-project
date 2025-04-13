93e89013a7adeae94d535029056dca9f9376b7cd
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2025-04-13 19:42:58 +0300

    slider with alice carousel

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,26 +1,62 @@
-import {FlexContainer} from '../FlexContainer.ts';
-import React from "react";
 import {S} from "./Slider_Styles.ts"
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import "./../../styles/slider.css"
+
+type SlideType = {
+    text: string
+    name: string
+}
+
+const Slide = ({text, name}: SlideType) => {
+    return <S.Slide>
+        <S.Text>{text}</S.Text>
+        <S.Name>@{name}</S.Name>
+    </S.Slide>
+}
 
 
-export const Slider: React.FC = () => {
-    return (
-        <S.Slider>
-            <FlexContainer align="center">
-                <S.Slide>
-                    <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore
-                        et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</S.Text>
-                    <S.Name>@ivan ivanow</S.Name>
-                </S.Slide>
-            </FlexContainer>
+const items = [
+    <Slide name={'ivan ivanow'}
+           text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore '+
+               'et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>,
+    <Slide name={'vasili vasiliev'}
+           text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore '+
+               'et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>,
+    <Slide name={'piotr petrov'}
+           text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore '+
+               'et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
 
-            <S.Pagination>
-                <span></span>
-                <span className={'active'}></span>
-                <span></span>
-            </S.Pagination>
+];
 
-        </S.Slider>
-    );
-};
+export const Slider = () => (
+    <S.Slider>
+        <AliceCarousel
+            mouseTracking
+            items={items}
+            disableButtonsControls
+        />
+    </S.Slider>
+);
+
+
+// export const Slider: React.FC = () => {
+//     return (
+//         <S.Slider>
+//             <FlexContainer align="center">
+//                 <S.Slide>
+//                     <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+//                         labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</S.Text>
+//                     <S.Name>@ivan ivanow</S.Name>
+//                 </S.Slide>
+//             </FlexContainer>
+//
+//             <S.Pagination>
+//                 <span></span>
+//                 <span className={'active'}></span>
+//                 <span></span>
+//             </S.Pagination>
+//
+//         </S.Slider>
+//     );
+// };
--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -24,3 +24,5 @@ export const Testimony: React.FC = () => {
     );
 };
 
+
+
